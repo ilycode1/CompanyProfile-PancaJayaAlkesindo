8d73373dde442da5f59c9658ebcd623a9354ad60
ilycode2@gmail.com <70672465+IlhamAbdul07@users.noreply.github.com>
2026-04-25 15:30:26 +0700

    test inject opsi 2

--- inject-env.js
+++ inject-env.js
@@ -3,12 +3,12 @@ import fs from "fs";
 let config = fs.readFileSync("./config.js", "utf-8");
 
 config = config
-  .replace("OMAILER_ENDPOINT", process.env.OMAILER_ENDPOINT)
-  .replace("SMTP_HOST", process.env.SMTP_HOST)
-  .replace("SMTP_PORT", process.env.SMTP_PORT)
-  .replace("SMTP_AUTH_EMAIL", process.env.SMTP_AUTH_EMAIL)
-  .replace("SMTP_AUTH_PASSWORD", process.env.SMTP_AUTH_PASSWORD)
-  .replace("SMTP_SENDER_NAME", process.env.SMTP_SENDER_NAME)
-  .replace("SMTP_RECIPIENT", process.env.SMTP_RECIPIENT);
+  .replace(/OMAILER_ENDPOINT/g, process.env.OMAILER_ENDPOINT || "")
+  .replace(/SMTP_HOST/g, process.env.SMTP_HOST || "")
+  .replace(/SMTP_PORT/g, process.env.SMTP_PORT || "")
+  .replace(/SMTP_AUTH_EMAIL/g, process.env.SMTP_AUTH_EMAIL || "")
+  .replace(/SMTP_AUTH_PASSWORD/g, process.env.SMTP_AUTH_PASSWORD || "")
+  .replace(/SMTP_SENDER_NAME/g, process.env.SMTP_SENDER_NAME || "")
+  .replace(/SMTP_RECIPIENT/g, process.env.SMTP_RECIPIENT || "");
 
 fs.writeFileSync("./config.js", config);
